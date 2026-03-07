2590d6a8132563e76604d73be909c20d9582acaa
Your Name <petrakliy@gmail.com>
2026-03-07 22:44:51 +0200

    add rose-pine theme

--- dot_config/nvim/lua/custom/plugins/colors.lua
+++ dot_config/nvim/lua/custom/plugins/colors.lua
@@ -1,12 +1,17 @@
 return {
-	"neanias/everforest-nvim",
-	version = false,
-	-- lazy = false,
-	-- priority = 1000, -- make sure to load this before all the other start plugins
-	-- Optional; default configuration will be used if setup isn't called.
-	config = function()
-		-- require("everforest").setup({
-		-- 	-- Your config here
-		-- })
-	end
+
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		config = function()
+			require("everforest").setup({
+				-- Your config here
+			})
+		end
+	},
+
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	}
 }
