5c83bb1971101570a3f30afb10299b15b4dd7f78
larikhide <petrakliy@gmail.com>
2026-03-24 15:43:04 +0200

    turn on lazy load for themes

--- dot_config/nvim/lua/custom/plugins/colors.lua
+++ dot_config/nvim/lua/custom/plugins/colors.lua
@@ -20,7 +20,7 @@ return {
 
 	{
 		"neanias/everforest-nvim",
-		lazy = false,
+		-- lazy = false,
 		-- priority = 1000, -- make sure to load this before all the other start plugins
 		-- Optional; default configuration will be used if setup isn't called.
 		config = function()
