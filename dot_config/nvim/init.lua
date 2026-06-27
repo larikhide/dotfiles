43ebe48a7df239253a199a946ce87216e0f407d9
Your Name <petrakliy@gmail.com>
2026-06-27 10:18:47 +0300

    nvim: close diagnostic window

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -104,6 +104,7 @@ vim.diagnostic.config({
 })
 
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>l", vim.cmd.lclose, { desc = "Close diagnostic quickfix [L]ist" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
